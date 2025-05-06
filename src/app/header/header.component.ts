da485c54d993e19e27edbf7adfcfacc3623dc9eb
Rissta <laptevka22@oiate.ru>
2025-05-06 16:39:53 +0300

    Развернул проект и сделал хедер

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+
+export class HeaderComponent {
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
+
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { UserManagementComponent } from '../user-management/user-management.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  imports: [HeaderComponent, SidebarComponent, UserManagementComponent]
+})
+export class LayoutComponent {}
